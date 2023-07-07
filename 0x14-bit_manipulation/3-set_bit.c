964bea0209da6115cf33a28baf0ec87881cb126f
zjaad <zjaadani@gmail.com>
2023-07-06 18:50:27 -0700

    correction

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -12,8 +12,8 @@ int set_bit(unsigned long int *n, unsigned int index)
   if (index >= sizeof(unsigned long int) * 8)
     return -1;
 
-  unsigned long int mask = 1 << index;
-  *
+  unsigned long int mask = 1UL  << index;
+  *n |= mask;
 
-  return res;
+  return 1;
 }
--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,9 +12,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
   if (index >= sizeof(unsigned long int) * 8)
     return -1;
 
-  unsigned long int mask = 1 << index;
-  mask = ~mask;
-  *n &= mask;
+  unsigned long int mask = 1UL  << index;
+ 
+  *n &= ~mask;
 
   return 1;
 }
