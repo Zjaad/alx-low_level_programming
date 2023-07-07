0d2831e29056e3505416d493aab84667bb7ebe06
zjaad <zjaadani@gmail.com>
2023-07-06 17:53:50 -0700

    correction of the first task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,18 +3,17 @@
 unsigned int binary_to_uint(const char *b)
 {
   unsigned int res = 0;
-  unsigned int i = 0;
 
   if (b == NULL)
     return 0;
 
-  while (b[i])
+  while (*b)
   {
-    if (b[i] != '0' && b[i] != '1')
+    if (*b != '0' && *b != '1')
       return 0;
 
-    res = res * 2 + (b[i] - '0');
-    i++;
+    res = res * 2 + (*b - '0');
+    b++;
   }
 
   return res;
