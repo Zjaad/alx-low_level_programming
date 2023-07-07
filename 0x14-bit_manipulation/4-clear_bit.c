a5f6c52f1ea10b8ea7df32051c36732b1c51f9a3
zjaad <zjaadani@gmail.com>
2023-07-06 19:06:32 -0700

    adding new file , correcting another one

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,12 +9,9 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-  if (index >= sizeof(unsigned long int) * 8)
-    return -1;
+	if (index > 63)
+		return (-1);
 
-  unsigned long int mask = 1UL  << index;
- 
-  *n &= ~mask;
-
-  return 1;
+	*n = (~(1UL << index) & *n);
+	return (1);
 }
