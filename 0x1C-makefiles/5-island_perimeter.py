2a4a0565c85c181278f4b1f5393a60f34a593259
zjaad <zjaadani@gmail.com>
2023-10-27 16:00:19 -0700

    adding files

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,8 +6,19 @@ def island_perimeter(grid):
     return: The perimeter of the island (grid).
     """
     if not grid:
-        return (0)
-    ze = grid[0]
-    x , y = len(grid) , len(ze)
-    e , s = 0, 0 
-
+        return (0) 
+    x, y = len(grid), len(grid[0])
+    p = 0
+    for i in range(x):
+        for j in range(y):
+            if grid[i][j] == 1:
+                p += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    p -= 1
+                if i < x - 1 and grid[i + 1][j] == 1:
+                    p -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    p -= 1
+                if j < y - 1 and grid[i][j + 1] == 1:
+                    p -= 1
+     return (p)
