bde183786e84967a752181fcf4979d128647818a
zjaad <zjaadani@gmail.com>
2024-01-24 16:14:23 -0800

    another commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,12 +4,15 @@ def island_perimeter(grid):
     """ It returns the perimeter of the island described in grid. """
     p = 0
     g, gz = len(grid) , len(grid[0])
-    for x in range(g):
-        for y in range(gz):
-            if grid[x][y] == 1:
-                p += 4
-                if x > 0 and grid[x-1][y] == 1:
-                    p -= 2
-                if y > 0 and grid[x][y - 1] == 1:
-                    p -= 2
-    return(p)
+    for x, u in enumerate(grid):
+        for y, w in enumerate(u):
+            if w == 1:
+                if x == 0 or grid[x - 1][y] != 1:
+                    p += 1
+                if y == 0 or grid[x][y - 1] != 1:
+                    p += 1
+                if y == gz - 1 or grid[x][y + 1] != 1:
+                    p += 1
+                if x == g - 1 or grid[x + 1][y] != 1:
+                    p += 1
+    return (p)
